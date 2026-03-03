7dd117f129835b87f03013e1564461a0c73725a6
JoshikaCB22 <145038800+JoshikaCB22@users.noreply.github.com>
2026-03-03 20:26:59 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private String category;
+
+    public Product(int id, String name, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d | %-20s | $%.2f | %s", id, name, price, category);
+    }
+}
